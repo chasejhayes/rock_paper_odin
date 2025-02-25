6765bf90f403776dd2e1fa7bdd16185bf50ec4c6
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2025-02-24 22:09:55 -0500

    Code 'playRound' function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 function getComputerChoice(){
-    choice = Math.random();
-    if (choice <= .33){
+    let computerChoice = Math.random();
+    if (computerChoice <= .33){
         return "rock"
     }
-    else if (choice > .33 && choice <= .66){
+    else if (computerChoice > .33 && choice <= .66){
         return "paper"
     }
-    else if (choice > .66){
+    else if (computerChoice > .66){
         return "scissors"
     }
 }
@@ -16,7 +16,40 @@ function getComputerChoice(){
 function getHumanChoice(){
     let humanChoice = prompt("Rock, Paper, or Scissors?");
     return humanChoice
-
 }
 
-console.log(getHumanChoice())
+// console.log(getHumanChoice())
+
+let computerScore = 0;
+
+let humanScore = 0;
+
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+
+function playRound(){
+   if ((humanChoice == "rock" && computerChoice == "rock") || (humanChoice == "paper" && computerChoice == "paper") || (humanChoice == "scissors" && computerChoice == "scissors")  ){
+    return console.log("It's a tie!")
+   }
+   else if (humanChoice=="rock" && computerChoice == "scissors") {
+    return console.log("Human wins")
+   }
+   else if (humanChoice=="rock" && computerChoice == "paper") {
+    return console.log("Computer wins")
+   }
+   else if (humanChoice == "paper" && computerChoice == "rock"){
+    return console.log("human wins")
+   }
+   else if (humanChoice=="paper" && computerChoice == "scissors") {
+    return console.log("computer wins")
+   }
+   else if (humanChoice=="scissors" && computerChoice == "rock") {
+    console.log("computer wins")
+   }
+   else if (humanChoice=="scissors" && computerChoice == "paper") {
+    return console.log("Human wins")
+   }
+}
+
+console.log(playRound())
+
