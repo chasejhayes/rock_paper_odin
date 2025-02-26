c349ce191e1319b42e7e6cdc8c5074259cbcd42f
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2025-02-25 20:29:06 -0500

    Fix errors in scoring function and playRound function

--- javascript.js
+++ javascript.js
@@ -18,53 +18,53 @@ function getHumanChoice(){
 
 
 let computerScore = 0;
-
 let humanScore = 0;
 
 let computerChoice = getComputerChoice();
 let humanChoice = getHumanChoice();
 
-// Issue: || and && need to be used with boolean values
-// (humanChoice == "rock" && computerChoice == "rock") || (humanChoice == "paper" && computerChoice == "paper") || (humanChoice == "scissors" && computerChoice == "scissors")  
+
 function playRound(){
    if (humanChoice == computerChoice){
-    return console.log("It's a tie!")
+    return "It's a tie!";
    }
    else if (humanChoice=="rock" && computerChoice == "scissors") {
-    return console.log("Human wins")
+    return "Human wins";
    }
    else if (humanChoice=="rock" && computerChoice == "paper") {
-    return console.log("Computer wins")
+    return "Computer wins";
    }
    else if (humanChoice == "paper" && computerChoice == "rock"){
-    return console.log("Human wins")
+    return "Human wins";
    }
    else if (humanChoice=="paper" && computerChoice == "scissors") {
-    return console.log("Computer wins")
+    return "Computer wins";
    }
    else if (humanChoice=="scissors" && computerChoice == "rock") {
-    return console.log("Computer wins")
+    return "Computer wins";
    }
    else if (humanChoice=="scissors" && computerChoice == "paper") {
-    return console.log("Human wins")
+    return "Human wins";
    }
 }
+
 
 let result = playRound();
 
 function scoring(){
-    if (result === "Human wins") {
-        return ++humanScore
+    if (result == "Human wins") {
+        ++humanScore;
+        return "Human wins!"
        }
-    else if (result === "Computer wins") {
-        return ++computerScore
+    else if (result == "Computer wins") {
+        ++computerScore
+        return "Computer wins!"
        }
     else {
         return "Nobody wins!"
     }
 }
 
-// console.log(playRound()); (Results in 'undefined')
 console.log(scoring());
 console.log(humanChoice);
 console.log(computerChoice);
