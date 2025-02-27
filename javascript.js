5cbe3c88a9823c462e103a0fc49149ab1dc0abc4
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2025-02-26 20:34:47 -0500

    Code game scoring and endGame function

--- javascript.js
+++ javascript.js
@@ -1,73 +1,100 @@
-function getComputerChoice(){
-    let choice = Math.random();
-    if (choice <= .33){
-        return "rock"
-    }
-    else if (choice > .33 && choice <= .66){
-        return "paper"
-    }
-    else if (choice > .66){
-        return "scissors"
-    }
-}
-
-function getHumanChoice(){
-    let choice = prompt("Rock, Paper, or Scissors?");
-    return choice;
-}
-
-
 let computerScore = 0;
 let humanScore = 0;
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
 
+function playGame(){
 
-function playRound(){
-   if (humanChoice == computerChoice){
-    return "It's a tie!";
-   }
-   else if (humanChoice=="rock" && computerChoice == "scissors") {
-    return "Human wins";
-   }
-   else if (humanChoice=="rock" && computerChoice == "paper") {
-    return "Computer wins";
-   }
-   else if (humanChoice == "paper" && computerChoice == "rock"){
-    return "Human wins";
-   }
-   else if (humanChoice=="paper" && computerChoice == "scissors") {
-    return "Computer wins";
-   }
-   else if (humanChoice=="scissors" && computerChoice == "rock") {
-    return "Computer wins";
-   }
-   else if (humanChoice=="scissors" && computerChoice == "paper") {
-    return "Human wins";
-   }
+    function getComputerChoice(){
+        let choice = Math.random();
+        if (choice <= .33){
+            return "rock"
+        }
+        else if (choice > .33 && choice <= .66){
+            return "paper"
+        }
+        else if (choice > .66){
+            return "scissors"
+        }
+    }
+    
+    function getHumanChoice(){
+        let choice = prompt("Rock, Paper, or Scissors?");
+        return choice;
+    }
+    
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+    
+    
+    function playRound(computerChoice, humanChoice){
+       if (humanChoice == computerChoice){
+        return "It's a tie!";
+       }
+       else if (humanChoice=="rock" && computerChoice == "scissors") {
+        return "Human wins";
+       }
+       else if (humanChoice == "paper" && computerChoice == "rock"){
+        return "Human wins";
+       }
+       else if (humanChoice=="scissors" && computerChoice == "paper") {
+        return "Human wins";
+       }
+       else {
+        return "Computer wins";
+       }
+    }
+    
+    
+    let result = playRound(computerChoice, humanChoice);
+    
+    
+    
+    function scoring(){
+        if (result == "Human wins") {
+            ++humanScore;
+            return "Human wins!"
+           }
+        else if (result == "Computer wins") {
+            ++computerScore;
+            return "Computer wins!"
+           }
+        else {
+            return "Nobody wins!"
+        }
+    }
+
+    console.log(scoring());
+    console.log(humanChoice);
+    console.log(computerChoice);
+    console.log(humanScore);
+    console.log(computerScore);
+
 }
 
-
-let result = playRound();
-
-function scoring(){
-    if (result == "Human wins") {
-        ++humanScore;
-        return "Human wins!"
-       }
-    else if (result == "Computer wins") {
-        ++computerScore
-        return "Computer wins!"
-       }
+function endGame(){
+    if (computerScore > humanScore){
+        return "Computer wins the game!"
+    }
+    else if (humanScore > computerScore){
+        return "Human wins the game!"
+    }
     else {
-        return "Nobody wins!"
+        return "The game is tied!"
     }
 }
 
-console.log(scoring());
-console.log(humanChoice);
-console.log(computerChoice);
-console.log(humanScore);
-console.log(computerScore);
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(endGame());
 
+
+
+
+
+
+
+// remake to switch statement
+// Make player imput case insensitive
